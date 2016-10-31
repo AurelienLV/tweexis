c3bec561d0df157eec8cbd2ed2f38a4a5d82e010
AurelienLV <aurelien.lucienvauthier@gmail.com>
2016-10-31 14:50:22 +0100

    ok

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
+    <?php
+        require_once("twitteroauth-master/autoload.php");
+        require_once("twitteroauth-master/src/TwitterOAuth.php"); //Path to twitteroauth library
+        session_start();
+    ?>
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -97,14 +102,45 @@
                 font-weight: bold;
                 font-size: 20px;
             }
+            th > a {
+                color: mediumvioletred;
+                font-size: 14px;
+                text-decoration: none;
+            }
+            td > img {
+                max-width: 70px;
+                max-height: 70px;
+            }
+            td > a {
+                color: black;
+            }
+            th, td {
+                padding: 0 10px;
+            }
+            .modal {
+                 display:    none;
+                 position:   fixed;
+                 z-index:    1000;
+                 top:        0;
+                 left:       0;
+                 height:     100%;
+                 width:      100%;
+                 background: rgba( 255, 255, 255, .8 ) 
+                             url('http://i.stack.imgur.com/FhHRx.gif') 
+                             50% 50% 
+                             no-repeat;
+            }
+            body.loading {
+                 overflow: hidden;   
+            }
+            body.loading .modal {
+                 display: block;
+            }
         </style>
     </head>
     <body>
         <h1>Tweexis</h1>
     <?php
-        require_once("twitteroauth-master/autoload.php");
-        require_once("twitteroauth-master/src/TwitterOAuth.php"); //Path to twitteroauth library
-        session_start();
         
         if (isset($_POST['pwd'])) {
             $hashed_password = '$1$DyEB00Pc$fpvjpnOLSdDfnjRXp3cxk0';
@@ -171,24 +207,31 @@
             <div id="content">
                 <div id="table_content">
                     <p id="count_fav">Nombre de favoris (existants) : <span class='count_fav'></span></p>
+                    
+                    <table id="table_accounts">
+                        <tr>
+                            <th></th>
+                            <th><a href="?sort=id&order=<?php if (isset($_GET['order']) && isset($_GET['sort']) && $_GET['sort'] == 'id' && $_GET['order'] == 'asc') { echo 'desc'; } else { echo 'asc'; } ?>">Identifiant</a></th>
+                            <th><a href="?sort=pseudo&order=<?php if (isset($_GET['order']) && isset($_GET['sort']) && $_GET['sort'] == 'pseudo' && $_GET['order'] == 'asc') { echo 'desc'; } else { echo 'asc'; } ?>">Pseudo</a></th>
+                            <th><a href="?sort=followers&order=<?php if (isset($_GET['order']) && isset($_GET['sort']) && $_GET['sort'] == 'followers' && $_GET['order'] == 'asc') { echo 'desc'; } else { echo 'asc'; } ?>">Nombre d'abonnés</a></th>
+                            <th><a href="?sort=friends&order=<?php if (isset($_GET['order']) && isset($_GET['sort']) && $_GET['sort'] == 'friends' && $_GET['order'] == 'asc') { echo 'desc'; } else { echo 'asc'; } ?>">Nombre d'abonnements</a></th>
+                            <th><a href="?sort=ratio&order=<?php if (isset($_GET['order']) && isset($_GET['sort']) && $_GET['sort'] == 'ratio' && $_GET['order'] == 'asc') { echo 'desc'; } else { echo 'asc'; } ?>">abonnés / abonnements</a></th>
+                        </tr>
+                    </table>
+                    
                     <form id="add_fav" action="index.php" method="POST">100 maximum
                         <textarea id="all_fav" name="all_fav" form="add_fav" rows="20" cols="50"></textarea><br/>
                         <input type="submit" id="add_submit" value="Editer" />
                     </form>
-                    <table id="table_accounts">
-                        <tr>
-                            <th><a href="?sort=id">Identifiant</a></th>
-                            <th><a href="?sort=pseudo">Pseudo</a></th>
-                            <th><a href="?sort=followers">Nombre d'abonnés</a></th>
-                            <th><a href="?sort=friends">Nombre d'abonnements</a></th>
-                            <th><a href="?sort=friends">abonnés / abonnements</a></th>
-                        </tr>
-                    </table>
                 </div>
             </div>
         </div>
+        <div class="modal"><!-- Place at bottom of page --></div>
         <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
         <script language="JavaScript">
+            var body = $("body");
+            $(document).ajaxStart(function() { body.addClass("loading"); });
+            $(document).ajaxStop(function() { body.removeClass("loading"); });
             $( document ).ready(function() {
                 init();
 
@@ -238,16 +281,18 @@
                             $('.count_fav').text(array.length);
                             array = sortList(getURLParameter("sort"), array);
                             $.each(array, function(i,value) {
-                                $('#table_accounts').append('<tr><td>@'+
+                                $('#table_accounts').append('<tr><td>'+
+                                    '<img src="'+value['profile_image_url']+'" />'
+                                +'</td><td><a target="_blank" href="http://www.twitter.com/'+value['screen_name']+'">@'+
                                     value['screen_name']
-                                +'</td><td>'+
+                                +'</a></td><td>'+
                                     value['name']
                                 +'</td><td>'+
                                     value['followers_count']
                                 +'</td><td>'+
                                     value['friends_count']
-                                +'</td></tr>'+
-                                    value['followers_count'] / value['friends_count']
+                                +'</td><td>'+
+                                    (parseFloat(value['followers_count']) / parseFloat(value['friends_count'])).toFixed(3)
                                 +'</td></tr>');
                             });
                         },
@@ -265,23 +310,46 @@
                     var newArray = array;
                     switch (criteria) {
                         case 'id':
-                            
+                            newArray.sort(SortBySName);
                             break;
                         case 'pseudo':
-                            
+                            newArray.sort(SortByName);
                             break;
                         case 'friends':
-                            
+                            newArray.sort(SortByFriends);
                             break;
                         case 'followers':
-                        
+                            newArray.sort(SortByFollow);
                             break;
                         case 'ratio':
-                            
+                            newArray.sort(SortByRatio);
                             break;
                     }
                     return newArray;
                 }
+                
+                function SortBySName(a, b) { return SortBy('screen_name', a, b); }
+                function SortByName(a, b) { return SortBy('name', a, b); }
+                function SortByFollow(a, b) { return SortBy('followers_count', a, b); }
+                function SortByFriends(a, b) { return SortBy('friends_count', a, b); }
+                function SortByRatio(a, b) {
+                    a['ratio'] = parseFloat(a['followers_count']) / parseFloat(a['friends_count']);
+                    b['ratio'] = parseFloat(b['followers_count']) / parseFloat(b['friends_count']);
+                    return SortBy('ratio', a, b);
+                }
+
+                function SortBy(criteria, a, b){
+                    var a2 = a[criteria];
+                    if (typeof a2 === 'string') a2 = a2.toLowerCase();
+                    var b2 = b[criteria];
+                    if (typeof b2 === 'string') b2 = b2.toLowerCase();
+                    var compare = ((a2 < b2) ? -1 : ((a2 > b2) ? 1 : 0));
+                    if (getURLParameter('order') === 'asc') {
+                        return compare;
+                    } else {
+                        return (-1)*compare;
+                    }
+                  }
 
                 function displayInfo(msg) {
                     $('.info').text(msg);
